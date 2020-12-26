684acc390a57d92d92c19bdbebd9b7fbb4129084
Fernando Barreto <Fernando Barreto>
2020-12-26 17:06:53 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Components/Checkout/Checkout';
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMOdmjIiW2QQqQ7thQeRmPVAVmJ4Zwzvk',
+  authDomain: 'clone-b1aad.firebaseapp.com',
+  projectId: 'clone-b1aad',
+  storageBucket: 'clone-b1aad.appspot.com',
+  messagingSenderId: '541819404390',
+  appId: '1:541819404390:web:c70962bf205848efc9eda7',
+  measurementId: 'G-2E561363SM',
+};
